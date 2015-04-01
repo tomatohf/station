36790f46fd6fff5c2b1102e3dc79ba9e32907c3e
Tomato <Tomato.HF@gmail.com>
2015-04-01 23:23:03 +0800

    create and bind socket

--- src/Server.hs
+++ src/Server.hs
@@ -3,4 +3,7 @@ module Server where
 import Network.Socket
 
 server host port = withSocketsDo $ do
+    sock <- socket AF_INET Datagram defaultProtocol
+    addr <- inet_addr host
+    bind sock $ SockAddrInet (PortNum $ read port) addr
     putStrLn $ "server " ++ host ++ ":" ++ port
