e6cc7278972fb5e6cf8abcb37389c2c98eccd48a
Tomato <Tomato.HF@gmail.com>
2015-04-02 22:48:04 +0800

    loop process received command

--- src/Server.hs
+++ src/Server.hs
@@ -1,9 +1,22 @@
 module Server where
 
+import Data.Map
 import Network.Socket
+
+type Car = String
+type Cars = Map SockAddr Car
+
+process command sender cars = (cars, "999 UNKNOWN")
+
+loop :: Socket -> Cars -> IO ()
+loop sock cars = do
+    (command, _, sender) <- recvFrom sock 1024
+    let (nextCars, response) = process command sender cars
+    sendTo sock response sender
+    loop sock nextCars
 
 server host port = withSocketsDo $ do
     sock <- socket AF_INET Datagram defaultProtocol
     addr <- inet_addr host
     bind sock $ SockAddrInet (PortNum $ read port) addr
-    putStrLn $ "server " ++ host ++ ":" ++ port
+    loop sock empty
