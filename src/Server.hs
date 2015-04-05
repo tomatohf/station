ed6eac7f5db6eeb4b88f5ce02731b6e73e3ac5c2
Tomato <Tomato.HF@gmail.com>
2015-04-05 23:55:59 +0800

    expire by time

--- src/Server.hs
+++ src/Server.hs
@@ -3,28 +3,39 @@
 module Server where
 
 import Data.List (stripPrefix)
-import Data.Map (Map, empty, toList, insert, delete)
+import Data.Map as Map (Map, empty, toList, insert, delete, filter)
 import Network.Socket
+import Data.Time.Clock
 import Util
 
 type Car = String
-type Cars = Map SockAddr Car
+type CarInfo = (Car, UTCTime)
+type Cars = Map SockAddr CarInfo
+
+-- in second
+expire = 64
 
 status200 = "200 OK"
 status400 = "400 Bad Request"
 
-showCar (sockAddr, car) = unwords $ show sockAddr : [car]
+showCar (sockAddr, (car, _)) = unwords $ show sockAddr : [car]
 showCars cars = unlines $ map showCar $ toList cars
 
-process (stripPrefix "JOIN" -> Just car) sender cars = (insert sender car cars, status200)
-process "QUIT" sender cars = (delete sender cars, status200)
-process "LIST" sender cars = (cars, unlines $ status200 : [showCars cars])
-process _ _ cars = (cars, status400)
+process (stripPrefix "JOIN" -> Just car) sender now cars =
+    (insert sender (car, now) cars, status200)
+process "QUIT" sender _ cars = (delete sender cars, status200)
+process "LIST" sender now cars =
+    (onlineCars, unlines $ status200 : [showCars onlineCars])
+  where
+    onlineCars = Map.filter online cars
+    online (_, when) = diffUTCTime now when < expire
+process _ _ _ cars = (cars, status400)
 
 loop :: Socket -> Cars -> IO ()
 loop sock cars = do
     (command, _, sender) <- recvFrom sock 1024
-    let (nextCars, response) = process (trim command) sender cars
+    now <- getCurrentTime
+    let (nextCars, response) = process (trim command) sender now cars
     sendTo sock response sender
     loop sock nextCars
 
